e541e2df2b5348a085365328257db88b1230a744
nwesem <niclaswesemann@gmail.com>
2022-08-11 11:15:41 +0200

    add bash files for signing uninstaller and creating zip
    Signed-off-by: nwesem <niclaswesemann@gmail.com>

---
+++ Installer/sign_uninstaller.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env zsh
+
+# Sign with Developer ID Application
+codesign --force --deep --options runtime --sign 4M658Z2K2X ../uninstall.command
---
+++ create_zip.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env zsh
+
+zip SiriusAudioDrivers.zip Installer/SiriusA.pkg Installer/SiriusB.pkg uninstall.command
+
